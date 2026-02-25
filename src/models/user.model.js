0d10f8df6637998d598933e633d6330bd8c78c84
Rakib Dhali <rakibdhali343@gmail.com>
2026-02-25 12:32:19 +0600

    email validation using regex

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,32 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required creating a user"],
+    trim: true,
+    lowercase: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "invalid email address",
+    ],
+    unique: [true, " Email already exists"]
+  },
+  name: {
+    type: String,
+    required: [true, " name is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "password required"],
+    minlength: [6, "password should be at least 6 character long "], 
+    select: false,
+  }
+}, {
+    timestamps: true
+  });
+
+
+userSchema.pre("save", async function(next){
     
 })
